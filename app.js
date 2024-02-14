923fe51f3901c2f3411c1566927f198a2844acf7
tf292201 <tf292201@gmail.com>
2024-02-14 07:43:11 -0500

    added functions for math

--- app.js
+++ app.js
@@ -1,27 +1,73 @@
 const express = require('express');
 const app = express();
+const errorEvent = require('./errorClass');
+
+
+
 
 // Route for calculating the mean
-app.get('/mean', (req, res) => {
-  // Logic to calculate the mean
-  // ...
+app.get('/mean', (req, res, next) => {
+    if (!req.query.nums) {
+        throw new errorEvent'You must pass a query key of nums with a comma-separated list of numbers', 400);
+    }
 
-  res.send(`Mean: ${mean}`);
+    const numbers = req.query.nums.split(',').map(Number);
+
+    if (numbers.some(isNaN)) {
+        throw new errorEvent('Values must be numbers', 400);
+    }
+
+    const sum = numbers.reduce((acc, num) => acc + num, 0);
+    const mean = sum / numbers.length;
+
+    res.send(`Mean: ${mean}`);
 });
+
+
 
 // Route for calculating the mode
-app.get('/mode', (req, res) => {
-  // Logic to calculate the mode
-  // ...
+app.get('/mode', (req, res, next) => {
+    if (!req.query.nums) {
+        throw new errorEvent('You must pass a query key of nums with a comma-separated list of numbers', 400);
+    }
 
-  res.send(`Mode: ${mode}`);
+    const numbers = req.query.nums.split(',').map(Number);
+
+    if (numbers.some(isNaN)) {
+        throw new errorEvent('Values must be numbers', 400);
+    }
+
+    let mode = 0;
+    let count = 0;
+    for (let num of numbers) {
+        let tempCount = 0;
+        for (let num2 of numbers) {
+            if (num === num2) {
+                tempCount++;
+            }
+        }
+        if (tempCount > count) {
+            mode = num;
+            count = tempCount;
+        }
+    }
+
+    res.send(`Mode: ${mode}`);
 });
+
 
 // Route for calculating the median
 app.get('/median', (req, res) => {
-  // Logic to calculate the median
-  // ...
-
+    if (!req.query.nums) {
+      throw new errorEvent('You must pass a query key of nums with a comma-separated list of numbers', 400);
+    }
+    numbers = req.query.nums.split(',').sort((a, b) => a - b);
+    let median = 0;
+    if (numbers.length % 2 === 0) {
+      median = (parseFloat(numbers[numbers.length / 2 - 1]) + parseFloat(numbers[numbers.length / 2])) / 2;
+    } else {
+      median = parseFloat(numbers[Math.floor(numbers.length / 2)]);
+    }
   res.send(`Median: ${median}`);
 });
 
